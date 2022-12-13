46832893587341cbadc4f852abe75c08edfc4d86
Loftystudios <loftyshadesstudio@gmail.com>
2022-12-12 20:58:02 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - print a given phrase
+ * return: 0 if properly exited
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+		return (0);
+}
