63b5031d6b793f92a116837e2d8ebdcd0e30545e
Loftystudios <loftyshadesstudio@gmail.com>
2022-12-14 18:09:50 -0800

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("size of char: %c bytes(s)\n", sizeof(char));
-	printf("size of int: %i bytes(s)\n", sizeof(int));
-	printf("size of long int: %li bytes(s)\n", sizeof(long int));
-	printf("size of long long int: %lli bytes(s)\n", sizeof(long long int));
-	printf("size of float: %f bytes(s)\n", sizeof(float));
+	printf("size of char: %d bytes(s)\n", sizeof(char));
+	printf("size of int: %d bytes(s)\n", sizeof(int));
+	printf("size of long int: %d bytes(s)\n", sizeof(long int));
+	printf("size of long long int: %d bytes(s)\n", sizeof(long long int));
+	printf("size of float: %d bytes(s)\n", sizeof(float));
 	return (0);
 }
