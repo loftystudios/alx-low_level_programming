0886320eab143f1eda6ebf50bce8cfb59005904b
Loftystudios <loftyshadesstudio@gmail.com>
2022-12-16 18:03:46 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num;
+	char lc;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (lc = 'a'; lc <= 'f'; lc++)
+		putchar(lc);
+
+	putchar('\n');
+
+	return (0);
+}
