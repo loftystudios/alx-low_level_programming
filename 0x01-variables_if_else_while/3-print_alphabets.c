496f9c39b54e309005b6e715fe40f7701cd0d91e
Loftystudios <loftyshadesstudio@gmail.com>
2022-12-16 16:07:36 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char lowerc = 'a';
+	char upperc = 'A';
+
+	for (lowerc == 'a'; 'a' <= 'z'; lowerc++)
+	{
+		putchar(lowerc);
+	}
+	while (upperc == 'A'; 'A' <= 'Z'; upperc++)
+	{
+		putchar(upperc);
+	}
+	putchar('\n')
+	
+	return (0);
+}
